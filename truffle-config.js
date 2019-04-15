217a2d377a8dd5fa8d7c65347e6ab20f6cc06de9
Joaquin Cardoso <joaquincardosobuzon@gmail.com>
2019-04-15 13:31:09 +0200

    [master] fix error in path of truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -27,7 +27,7 @@
 const path = require('path');
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, tic-tac/src/contracts),
+  contracts_build_directory: path.join(__dirname, 'tic-tac/src/contracts'),
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -88,7 +88,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.5.6",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
