555dae3090da6370b8b49ccccf56aea52e8c986e
Joaquin Cardoso <joaquincardosobuzon@gmail.com>
2019-04-15 13:30:34 +0200

    [master] drizzleOptions added

---
+++ tic-tac/src/drizzleOptions.js
@@ -0,0 +1,17 @@
+import TicTac from "./contracts/TicTac.json";
+
+const options = {
+    web3: {
+        block: false,
+        fallback: {
+            type: "ws",
+            url: "ws://127.0.0.1:8545",
+        },
+    },
+    contracts: [TicTac],
+    polls: {
+        accounts: 1500,
+    }
+};
+
+export default options;
